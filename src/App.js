187b5c8fa977a344ec0d3b206b90699509e56b89
Edison <edirates@gmail.com>
2019-11-25 16:52:34 +0700

    Implement react hooks in app.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState, useEffect } from 'react';
 import Search from './Search';
 import List from './List';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      digimons: [],
-      search: ""
-    }
+const App = () => {
+  const [ digimons, setDigimons ] = useState([]);
+  const [ search, setSearch ] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
   }
-  handleChange = (e) => {
-    this.setState({
-      search: e.target.value
-    }, function () {
-      // console.log(this.state.search);
-    });
-  }
-  componentDidMount() {
+
+  useEffect(() => {
     fetch("https://digimon-api.herokuapp.com/api/digimon")
     .then( response => response.json() )
     .then( (digimons) => {
-      this.setState({
-        digimons: digimons
-      })
+      setDigimons(digimons)
     })
-  }
-  render() {
-    return (
-      <Fragment>
-        <Search search={this.state.search} change={this.handleChange}/>
-        <List search={this.state.search} digimons={this.state.digimons.slice(0, 12)} />
-      </Fragment>
-    );
-  }
+  }, []);
+
+  return (
+    <Fragment>
+      <Search search={search} change={handleChange}/>
+      <List search={search} digimons={digimons.slice(0, 12)} />
+    </Fragment>
+  );
 }
+
+// ======== Class Method ========
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       digimons: [],
+//       search: ""
+//     }
+//   }
+//   handleChange = (e) => {
+//     this.setState({
+//       search: e.target.value
+//     }, function () {
+//       // console.log(this.state.search);
+//     });
+//   }
+//   componentDidMount() {
+//     fetch("https://digimon-api.herokuapp.com/api/digimon")
+//     .then( response => response.json() )
+//     .then( (digimons) => {
+//       this.setState({
+//         digimons: digimons
+//       })
+//     })
+//   }
+//   render() {
+//     return (
+//       <Fragment>
+//         <Search search={this.state.search} change={this.handleChange}/>
+//         <List search={this.state.search} digimons={this.state.digimons.slice(0, 12)} />
+//       </Fragment>
+//     );
+//   }
+// }
 
 export default App;
